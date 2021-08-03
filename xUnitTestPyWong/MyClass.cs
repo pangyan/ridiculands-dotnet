af473a27747f6d706f3c74cea8c7e8f4f3e8c151
Pangyan Wong <pangyan.wong@gmail.com>
2021-08-03 15:22:52 +0800

    CLS-167 - dotnet starter
    dotnet starter - C# xunit tests

---
+++ xUnitTestPyWong/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace xUnitTestPyWong
+{
+    public class MyClass
+    {
+        String a = "diu";
+
+        public MyClass()
+        {
+            Console.WriteLine("Constructor...");
+        }
+
+        public void dnlm()
+        {
+            a = "hahaha";
+
+            // test unit test delay
+            Thread.Sleep(5000);
+            a = "Hello World!";
+        }
+
+        public String getA()
+        {
+            return a;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello Ridiculands");
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.a);
+            myClass.dnlm();
+            Console.WriteLine(myClass.a);
+        }
+    }
+}
---
+++ xUnitTestPyWong/UnitTest2.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+using xUnitTestPyWong;
+
+namespace HelloWorldTests
+{
+    public class Tests
+    {
+        private const string Expected = "Hello World!";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            MyClass myClass = new MyClass();
+            myClass.dnlm();
+            Assert.That(() => myClass.getA(), Is.EqualTo(Expected).After(10).Seconds.PollEvery(250).MilliSeconds);
+        }
+    }
+}
